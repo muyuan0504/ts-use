a6d95a0c700fd7e0a2a50ade851a3be2efa739b3
jimouspeng <jimouspeng@xiaoe-tech.com>
2022-04-27 16:41:44 +0800

    fix

--- index.ts
+++ index.ts
@@ -33,6 +33,7 @@ let list0: Array<any> = [1, '2', { a: 0 }]
 let list1: Array<number> = [1, 2, 3]
 let list2: any[] = [1, 2, 3, '4']
 
+/**  :number 表示函数返回number */
 function getUser(): number {
     let b: Array<number> = [12, 2]
     return 1
@@ -51,7 +52,9 @@ let a: any;   // 执行tsc index.ts后，会将声明方式改用var
 
 
 
-/** 接口 */
+/** 【接口】
+ *  描述带有属性的普通对象外，接口也可以描述函数类型
+ */
 
 interface UserInfo {
     name: string,
@@ -69,6 +72,46 @@ function printLabel(labelledObj: UserInfo) {
 let myObj = { name: 'jimous', company: 'xiaoe', aList: [1, 2] };
 printLabel(myObj);
 
+
+interface SquareConfig {
+    color?: string;
+    width?: number;
+    [propName: string]: any; // 更好的方式绕过检查
+}
+
+function createSquare(config: SquareConfig): { color: string; area: number } {
+    // ...
+    console.log(config.color);
+    return {
+        color: config.color,
+        area: 12
+    }
+
+}
+
+createSquare({ colour: "red", width: 100 }); // 由于colour属性未定义，所以会提示错误
+// let mySquare = createSquare({ colour: "red", width: 100 } as SquareConfig); // 绕开这些检查非常简单。 最简便的方法是使用类型断言
+
+interface SearchFunc {
+    (source: string, subString: string): boolean;
+}
+let mySearch: SearchFunc;
+mySearch = function (src, sub) { // 对于函数类型的类型检查来说，函数的参数名不需要与接口里定义的名字相匹配
+    let result = src.search(sub);
+    return result > -1;
+}
+
+mySearch('', '')
+
+/** 可索引的类型-索引签名 */
+interface StringArray {
+    [index: number]: string; // 描述了对象索引的类型，还有相应的索引返回值类型
+}
+
+let myArray: StringArray;
+myArray = ["Bob", "Fred", 3];
+
+let myStr: string = myArray[0];
 
 export { }
 
